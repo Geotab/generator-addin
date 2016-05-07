514903ee134261973f46c733ca66b74907253a69
Beefo <beefo@hotmail.com>
2016-05-06 22:12:07 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# generator-addin [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url] [![Coverage percentage][coveralls-image]][coveralls-url]
+> MyGeotab/Geotab Drive add-in generator
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-addin using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-addin
+```
+
+Then generate your new project:
+
+```bash
+yo addin
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+Apache-2.0 © [Geotab Inc](https://github.com/geotab)
+
+
+[npm-image]: https://badge.fury.io/js/generator-addin.svg
+[npm-url]: https://npmjs.org/package/generator-addin
+[travis-image]: https://travis-ci.org/geotab/generator-addin.svg?branch=master
+[travis-url]: https://travis-ci.org/geotab/generator-addin
+[daviddm-image]: https://david-dm.org/geotab/generator-addin.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/geotab/generator-addin
+[coveralls-image]: https://coveralls.io/repos/geotab/generator-addin/badge.svg
+[coveralls-url]: https://coveralls.io/r/geotab/generator-addin
---
+++ generators/app/templates/app/scripts/main.js
@@ -0,0 +1,58 @@
+/**
+ * @returns {{initialize: Function, focus: Function, blur: Function}}
+ */
+geotab.addin.myaddin = function() {
+	'use strict';
+
+	// the root container
+	var elAddin;
+
+	return {
+		/**
+		 * initialize() is called only once when the Add-In is first loaded. Use this function to initialize the
+		 * Add-In's state such as default values or make API requests (MyGeotab or external) to ensure interface
+		 * is ready for the user.
+		 * @param {object} freshApi - The GeotabApi object for making calls to MyGeotab.
+		 * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
+		 * @param {function} initializeCallback - Call this when your initialize route is complete. Since your initialize routine
+		 *        might be doing asynchronous operations, you must call this method when the Add-In is ready
+		 *        for display to the user.
+		 */
+		initialize: function(freshApi, freshState, initializeCallback) {
+			elAddin = document.querySelector('#<%= root %>');
+			// MUST call initializeCallback when done any setup
+			initializeCallback();
+		},
+
+		/**
+		 * focus() is called whenever the Add-In receives focus.
+		 *
+		 * The first time the user clicks on the Add-In menu, initialize() will be called and when completed, focus().
+		 * focus() will be called again when the Add-In is revisited. Note that focus() will also be called whenever
+		 * the global state of the MyGeotab application changes, for example, if the user changes the global group
+		 * filter in the UI.
+		 *
+		 * @param {object} freshApi - The GeotabApi object for making calls to MyGeotab.
+		 * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
+		 */
+		focus: function(freshApi, freshState) {
+			// example of setting url state
+			freshState.setState({hello: 'world'});
+			// show main content
+			elAddin.className = '';
+		},
+
+		/**
+		 * blur() is called whenever the user navigates away from the Add-In.
+		 *
+		 * Use this function to save the page state or commit changes to a data store or release memory.
+		 *
+		 * @param {object} freshApi - The GeotabApi object for making calls to MyGeotab.
+		 * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
+		 */
+		blur: function() {
+			// hide main content
+			elAddin.className = 'hidden';
+		}
+	};
+};
