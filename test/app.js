49a87db5fa72dfbd4fa38fb57f442a90a2c037c7
Beefo <beefo@hotmail.com>
2016-05-14 13:27:33 -0400

    update generic app test

--- test/app.js
+++ test/app.js
@@ -6,13 +6,24 @@ var helpers = require('yeoman-test');
 describe('generator-addin:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+        name: 'my addin'
+      })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'dummyfile.txt'
+      'bower.json',
+      'gulpfile.babel.js',
+      'package.json',
+      'app/myAddin.html',
+      'app/config.json',
+      'app/images/icon.svg',
+      'app/scripts/main.js',
+      'app/styles/main.css',
+      'test/functional/test.js',
+      'test/functional/mocks/mocks.js'
     ]);
   });
 });
