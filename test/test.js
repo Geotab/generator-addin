4a69d5b235ac03eb0ebe3a14ad45072519ed67b3
Steve Hansen <stevehansen@geotab.com>
2016-05-16 14:16:49 -0400

    add test for test.js file

---
+++ test/test.js
@@ -0,0 +1,28 @@
+'use strict';
+var path = require('path');
+var assert = require('yeoman-assert');
+var helpers = require('yeoman-test');
+var fs = require('fs');
+
+describe('generator-addin:test.js', function () {
+  var props = {
+    name: 'my addin',
+    path: 'DriveAppLink/'
+  }, js;
+
+  before(function (done) {
+    helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts(props)
+      .on('end', function () {
+        js = fs.readFileSync('test/functional/test.js', 'utf8');
+        done();
+      });
+  });
+
+  it('has correct selector', function () {
+    assert.equal(true, js.indexOf('#myAddin') > -1, 'Has correct selector');
+  });
+  it('has device select for drive', function () {
+    assert.equal(true, js.indexOf('// select a device (only part of local add-in debugging)') > -1, 'Has device select for drive');
+  });
+});
