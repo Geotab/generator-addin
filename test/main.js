53f09cf82c0e0688b87828b4443310ebb5c91188
Steve Hansen <stevehansen@geotab.com>
2016-05-16 14:16:34 -0400

    make tests a little more clear

--- test/main.js
+++ test/main.js
@@ -4,20 +4,23 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 var fs = require('fs');
 
-describe('generator-addin:app', function () {
+describe('generator-addin:main.js', function () {
   var props = {
     name: 'my addin'
-  };
+  }, js;
 
   before(function (done) {
     helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts(props)
-      .on('end', done);
+      .on('end', function () {
+        js = fs.readFileSync('app/scripts/main.js', 'utf8');
+        done();
+      });
   });
-
-  it('creates config', function () {
-    var js = fs.readFileSync('app/scripts/main.js', 'utf8');
+  it('has correct name space', function () {
     assert.equal(true, js.indexOf('geotab.addin.myAddin') > -1);
+  });
+  it('has correct root identifier', function () {
     assert.equal(true, js.indexOf('#myAddin') > -1);
   });
 });
