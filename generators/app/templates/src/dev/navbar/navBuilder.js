dec7a167dfc2ac5b9ad2ff84113ad3c5829b1177
Nolan Kingdon <nolankingdon@geotab.com>
2020-02-03 09:17:57 -0500

    Updated yeoman logic to conditionally generate the navbar

--- generators/app/templates/src/dev/navbar/navBuilder.js
+++ generators/app/templates/src/dev/navbar/navBuilder.js
@@ -1,11 +1,13 @@
+<% if (!isButton && !isDriveAddin) { %>
 import "./navbar"; // Lay out the base to the main page;
+<% } %>
 import NavFactory from './NavFactory';
 import NavHandler from './NavHandler';
 import props from "./props";
 
 let factory = new NavFactory();
 let handler = new NavHandler(factory, props);
-<% if (!isButton) { %>
+<% if (!isButton && !isDriveAddin) { %>
 handler.generateContent();
 <% } %>
 handler.enableDisplayToggle();
