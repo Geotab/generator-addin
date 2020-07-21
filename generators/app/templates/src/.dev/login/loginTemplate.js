fd8c97124eed4cdafeb9e0f003776e49bb071f1a
Nolan Kingdon <nolankingdon@geotab.com>
2020-07-21 14:25:52 -0400

    Added in framework for groups - addin page only

--- generators/app/templates/src/.dev/login/loginTemplate.js
+++ generators/app/templates/src/.dev/login/loginTemplate.js
@@ -1,5 +1,6 @@
 let body = document.getElementsByTagName('body')[0];
 import icon from '../images/Font_Awesome_5_solid_chevron-left.svg';
+import xIcon from '../images/close-round.svg';
 import addinIcon from '../../app/images/icon.svg';
 let loginExample = `
 <% if (isDriveAddin) {%>
@@ -63,13 +64,106 @@ let loginExample = `
     }
 
     .dev-header {
-        
+        display: flex;
         background-color: #eee;
         flex-direction: row;
-        flax-wrap: wrap;
+        flex-wrap: wrap;
         justify-content: flex-end;
-        display: flex;
         border-bottom: 1px solid #ccc;
+    }
+
+    #group-wrapper {
+        position: absolute;
+        left: 250px;
+        display: flex;
+        flex-direction: row;
+        flex-wrap: nowrap;
+        flex-grow: 5;
+    }
+
+    #group-toggle-button svg {
+        mask-image: url(${icon});
+        mask-repeat: no-repeat;
+        -webkit-mask-image: url(${icon});
+        -webkit-mask-repeat: no-repeat;
+        background-color: #666;
+        transform: rotate(-90deg);
+    }
+
+    #group-selector {
+        display: flex;
+        position: relative;
+        z-index: 10003;
+        margin: 0.5em 0;
+    }
+
+    #group-input {
+        border: none;
+        z-index: 10003;
+        text-decoration: none;
+    }
+
+    #group-input:focus {
+        outline-style: none;
+    }
+
+    #group-input::placeholder {
+        font-weight: bold;
+        text-decoration: none;
+    }
+
+    #active-group {
+        padding: 5px 10px;
+        margin: 0.5em 0.5em 0.5em 0;
+        font-weight: bold;
+        color: #666;
+    }
+
+    .navButton {
+        display: block;
+    }
+
+    .select-buttons {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    .select-buttons li {
+        margin: 3px 10px 0px 10px;
+    }
+
+    #group-dropdown {
+        display: none;
+        position: absolute;
+        top: 40px;
+        width: 250px;
+        z-index: 10003;
+        min-height: auto;
+        max-height: 80vh;
+        overflow-y: auto;
+        border: 1px solid #ccc;
+    }
+
+    #group-remove-all {
+        padding: 5px 10px;
+        display: none;
+        border: none;
+        width: 20px;
+        height: 20px;
+        cursor: pointer;
+        margin: 0.5em 0.5em 0.5em 0;
+    }
+
+    #group-remove-all:focus {
+        outline: none;
+    }
+
+    #group-remove-all svg {
+        mask-image: url(${xIcon});
+        mask-repeat: no-repeat;
+        -webkit-mask-image: url(${xIcon});
+        -webkit-mask-repeat: no-repeat;
+        background-color: #666;
     }
 
     .dev-button {
@@ -143,9 +237,24 @@ let loginExample = `
         </label>
         <a id="toggleBtn" class="dev-button">Blur add-in</a>
       <% } else { %>
+        <div id="group-wrapper">
+            <div id="group-selector" class="geotabFormEditField noTranslate">
+                <input type="text" id="group-input" placeholder="Search for Groups">
+            </div>
+            <button id="group-toggle-button" class="dev-button">
+                <svg class="svgIcon geotabIcons_chevron" style="height: 15px; width: 15px;"></svg>
+            </button>
+            <div id="active-group">
+                Active Groups: All
+                </div>
+            <button id="group-remove-all">
+                <svg class="svgIcon" style="height: 15px; width: 15px"></svg>
+            </button>
+            <div id="group-dropdown" class="geotabPrimaryFill"></div>
+        </div>
         <div id="languages-target"></div>
         <% if (!isButton) {%>
-            <a id="toggleBtn" class="dev-button">Blur add-in</a>
+        <a id="toggleBtn" class="dev-button">Blur add-in</a>
         <% } %>
         <a target="_blank" href="src/.dev/styles/styleGuideMyGeotab.html" class="dev-button">Style Guide</a>
       <% } %>
